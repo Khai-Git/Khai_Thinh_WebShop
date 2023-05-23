78734a7738851b9d0e282a184419710b981572e4
Khai-Git <kelvinnon28092001@gmail.com>
2023-05-23 23:47:37 +0700

    Refix code: Fix create Account form into open_Modal_Create_Account

--- js/script.js
+++ js/script.js
@@ -25,6 +25,15 @@ document.querySelector('#login-btn').onclick = () =>{
     navbar.classList.remove('active');
 }
 
+let signupForm = document.querySelector('.signup-form');
+
+document.querySelector('#signup-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
@@ -59,55 +68,56 @@ function prev(){
 var sign_up = document.getElementById('dangky')
 
 sign_up.onclick() = function () {
-    let tbl = document.getElementById('ds_DangKy')
-    let row = tbl.inserRow()
-    let cell1 = row.insertCell()
-    let cell2 = row.insertCell()
-    let cell3 = row.insertCell()
-    let cell4 = row.insertCell()
-    let cell5 = row.insertCell()
+    // let tbl = document.getElementById('ds_DangKy')
+    // let row = tbl.inserRow()
+    // let cell1 = row.insertCell()
+    // let cell2 = row.insertCell()
+    // let cell3 = row.insertCell()
+    // let cell4 = row.insertCell()
+    // let cell5 = row.insertCell()
 
-    cell1.innerHTML = document.getElementById('tendangnhap').value
+    // cell1.innerHTML = document.getElementById('tendangnhap').value
     
     let hvt = document.getElementById('hovaten').value
-    let check_hvt = /[A-Z]+[a-z]+\s+/g
+    let check_hvt = /^[a-zA-z ]+$/g
     
-    if (hvt.length == 0 || !(hvt.match(check_hvt))) {
+    if (hvt.test(check_hvt)) {
         alert('Họ và tên không hợp lệ')
-    } else {
-        cell2.innerHTML = document.getElementById('hovaten').value
     }
+    // else {
+    //     cell2.innerHTML = document.getElementById('hovaten').value
+    // }
     
-    let sdt = document.getElementById('sodienthoai').value
-    let check_sdt_10 = /\d{10}/g
-    let check_sdt_11 = /\d{11}/g
+    // let sdt = document.getElementById('sodienthoai').value
+    // let check_sdt_10 = /\d{10}/g
+    // let check_sdt_11 = /\d{11}/g
 
-    if (sdt.length == 0 || (!(sdt.match(check_sdt_10)) && !(sdt.match(check_sdt_11)))) {
-        alert('Số điện thoại không hợp lệ')
-    } else {
-        cell3.innerHTML = document.getElementById('sodienthoai').value
-    }
+    // if (sdt.length == 0 || (!(sdt.match(check_sdt_10)) && !(sdt.match(check_sdt_11)))) {
+    //     alert('Số điện thoại không hợp lệ')
+    // } else {
+    //     cell3.innerHTML = document.getElementById('sodienthoai').value
+    // }
 
-    let email = document.getElementById('email').value
-    let check_email = /[a-z]+\d+@[a-z]+[.].+/g
-    if (email.length == 0 || !(email.match(check_email))) {
-        alert('Email vừa nhập không hợp lệ')
-    } else {
-        cell4.innerHTML = document.getElementById('email').value
-    }
-    let pass = document.getElementById('password').value
-    let passrepeat = document.getElementById('passrepeat').value
+    // let email = document.getElementById('email').value
+    // let check_email = /[a-z]+\d+@[a-z]+[.].+/g
+    // if (email.length == 0 || !(email.match(check_email))) {
+    //     alert('Email vừa nhập không hợp lệ')
+    // } else {
+    //     cell4.innerHTML = document.getElementById('email').value
+    // }
+    // let pass = document.getElementById('password').value
+    // let passrepeat = document.getElementById('passrepeat').value
 
-    if (passrepeat != pass) {
-        alert('Mật khẩu không trùng khớp')
-    } else {
-        cell5.innerHTML = document.getElementById('passrepeat').value
-    }
+    // if (passrepeat != pass) {
+    //     alert('Mật khẩu không trùng khớp')
+    // } else {
+    //     cell5.innerHTML = document.getElementById('passrepeat').value
+    // }
 
-    document.getElementById('tendangnhap').value = ""
-    document.getElementById('hovaten').value = ""
-    document.getElementById('sodienthoai').value = ""
-    document.getElementById('email').value = ""
-    document.getElementById('password').value = ""
-    document.getElementById('passrepeat').value = ""
+    // document.getElementById('tendangnhap').value = ""
+    // document.getElementById('hovaten').value = ""
+    // document.getElementById('sodienthoai').value = ""
+    // document.getElementById('email').value = ""
+    // document.getElementById('password').value = ""
+    // document.getElementById('passrepeat').value = ""
 }
